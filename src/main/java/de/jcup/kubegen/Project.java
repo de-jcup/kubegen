4cd3cb42b1af6b94ede52eed958621ad298b5f50
de-jcup <albert.tregnaghi@gmail.com>
2019-02-08 12:54:02 +0100

    closes #2
    - npe no longer happens, instead log entry is done

--- src/main/java/de/jcup/kubegen/Project.java
+++ src/main/java/de/jcup/kubegen/Project.java
@@ -9,16 +9,21 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Project {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Project.class);
 
 	private File projectFolder;
 	private String name;
 	private List<String> environments = new ArrayList<>();
-	private Map<String,Map<String,String>> map = new TreeMap<>();
-	
+	private Map<String, Map<String, String>> map = new TreeMap<>();
+
 	public Project(File rootFolder, String name) {
 		this.name = name;
-		this.projectFolder = new File(rootFolder,name);
+		this.projectFolder = new File(rootFolder, name);
 	}
 
 	public String getName() {
@@ -28,62 +33,66 @@ public class Project {
 	public List<String> getEnvironments() {
 		return environments;
 	}
-	
 
 	public File getProjectFolder() {
 		return projectFolder;
 	}
 
 	public File getTemplateFolder() {
-		return new File(projectFolder,"templates");
+		return new File(projectFolder, "templates");
 	}
 
 	public File getValuesFolder() {
-		return new File(projectFolder,"values");
+		return new File(projectFolder, "values");
 	}
 
 	public File getValuesFile() {
-		return new File(getValuesFolder(),"values.properties");
+		return new File(getValuesFolder(), "values.properties");
 	}
-	
+
 	public File getValuesFile(String environment) {
-		return new File(getValuesFolder(),"values_"+environment+".properties");
+		return new File(getValuesFolder(), "values_" + environment + ".properties");
 	}
 
 	public String getValue(String key) {
-		return getValue(null,key);
+		return getValue(null, key);
 	}
 
 	public String getValue(String environment, String key) {
-		if (environment==null) {
-			environment="";
+		if (environment == null) {
+			environment = "";
 		}
-		Map<String, String> envMap = map.computeIfAbsent(environment, e-> new HashMap<>());
+		Map<String, String> envMap = map.computeIfAbsent(environment, e -> new HashMap<>());
 		String result = envMap.get(key);
-		if (result==null && ! key.isEmpty()) {
+		if (result == null && !key.isEmpty()) {
 			/* fallback to common value when not defined */
-			return getValue("",key);
+			return getValue("", key);
 		}
 		return result;
 	}
-	
+
 	public void putValue(String key, String value) {
-		putValue(null,key,value);
+		putValue(null, key, value);
 	}
 
 	public String putValue(String environment, String key, String value) {
-		if (environment==null) {
-			environment="";
+		if (environment == null) {
+			environment = "";
 		}
-		Map<String, String> envMap = map.computeIfAbsent(environment, e-> new HashMap<>());
-		return envMap.put(key,value);
+		Map<String, String> envMap = map.computeIfAbsent(environment, e -> new HashMap<>());
+		return envMap.put(key, value);
 	}
 
 	public Set<String> getCommonAndEnvKeys(String environment) {
 		Set<String> set = new TreeSet<>();
 		set.addAll(map.get("").keySet());
-		if (environment!=null && !environment.isEmpty()){
-			set.addAll(map.get(environment).keySet());
+		if (environment != null && !environment.isEmpty()) {
+			Map<String, String> envMap = map.get(environment);
+			if (envMap != null) {
+				set.addAll(envMap.keySet());
+			} else {
+				LOG.warn("Did not found environment map for '{}' so ignored", environment);
+			}
 		}
 		return set;
 	}
