477a9e3120121dfb8f3020bc23ae80d8b3c7a844
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2025-07-18 13:37:04 +0200

    Upgrade gradle + dependencies + wrote test #13

--- src/test/java/de/jcup/kubegen/scaffolding/ScaffoldGeneratorTest.java
+++ src/test/java/de/jcup/kubegen/scaffolding/ScaffoldGeneratorTest.java
@@ -1,48 +1,48 @@
 package de.jcup.kubegen.scaffolding;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import de.jcup.kubegen.Project;
 import de.jcup.kubegen.ProjectImporter;
 
 public class ScaffoldGeneratorTest {
 
-	private ScaffoldGenerator generatorToTest;
+    private ScaffoldGenerator generatorToTest;
 
-	@Before
-	public void before() {
-		generatorToTest=new ScaffoldGenerator();
-	}
-	
-	@Test
-	public void test_generate_and_reload() throws Exception{
-		File testRoot = Files.createTempDirectory("kubegen-test").toFile();
-		testRoot.deleteOnExit();
-		
-		Project project = new Project(testRoot, "name1");
-		project.getEnvironments().addAll(Arrays.asList("prod","int","dev"));
-		
-		generatorToTest.generate(project);
-		
-		ProjectImporter importer = new ProjectImporter();
-		
-		Project project2 = importer.importProject(testRoot, "name1");
-		
-		assertEquals("name1", project2.getName());
-		
-		Set<String> environments = project2.getEnvironments();
-		assertTrue(environments.contains("prod"));
-		assertTrue(environments.contains("int"));
-		assertTrue(environments.contains("dev"));
-	}
+    @BeforeEach
+    void before() {
+        generatorToTest = new ScaffoldGenerator();
+    }
+
+    @Test
+    void test_generate_and_reload() throws Exception {
+        File testRoot = Files.createTempDirectory("kubegen-test").toFile();
+        testRoot.deleteOnExit();
+
+        Project project = new Project(testRoot, "name1");
+        project.getEnvironments().addAll(Arrays.asList("prod", "int", "dev"));
+
+        generatorToTest.generate(project);
+
+        ProjectImporter importer = new ProjectImporter();
+
+        Project project2 = importer.importProject(testRoot, "name1");
+
+        assertEquals("name1", project2.getName());
+
+        Set<String> environments = project2.getEnvironments();
+        assertTrue(environments.contains("prod"));
+        assertTrue(environments.contains("int"));
+        assertTrue(environments.contains("dev"));
+    }
 
 }
